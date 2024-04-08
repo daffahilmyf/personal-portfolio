91486c79deda3093982bdace1e3ebe797cff1b01
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2024-04-08 16:42:11 +0700

    [ADD] jenkinsfile for CI/CD

--- backend/src/users/repository.rs
+++ backend/src/users/repository.rs
@@ -2,12 +2,13 @@ use std::sync::Arc;
 
 use anyhow::{Context, Result};
 use axum::async_trait;
+use mockall::automock;
 use crate::utils::database::Database;
 use super::{model::User, query::{GET_USERS, GET_USER_BY_SLUG}};
 
 pub type DynUsersRepository = Arc<dyn UserRepository + Send + Sync>;
 
-
+#[automock]
 #[async_trait]
 pub trait UserRepository {
     async fn find_by_slug(&self, slug: &str) -> Result<Option<User>>;
