c80ae498830754e39886495c44a07a0ce8e497af
reykim7854 <reykim7124@proton.me>
2024-06-18 19:47:46 +0700

    feat:add card component

---
+++ frontend/src/lib/components/card/Card.svelte
@@ -0,0 +1,21 @@
+<script>
+	/** @type {boolean} */
+	export let dropShadow = true;
+
+	/** @type {boolean} */
+	export let rounded = true;
+</script>
+
+<article class="bg-white p-4 {rounded ? 'rounded-md' : ''} {dropShadow ? 'drop-shadow-md' : ''}">
+	<header>
+		<slot name="header" />
+	</header>
+
+	<div>
+		<slot name="main" />
+	</div>
+
+	<footer>
+		<slot name="footer" />
+	</footer>
+</article>
